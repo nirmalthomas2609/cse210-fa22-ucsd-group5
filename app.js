fc85275a0463af7a73872f47eccb4a4ac5a9f969
anshulshah99 <ayshah@ucsd.edu>
2022-11-13 11:55:37 -0800

    Fixed syntax errors

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-var express = require("express");
+let express = require("express");
 
-var app = express();
+let app = express();
 
-app.get("/", function (req, res) {
-    res.send("Simple Web Application is UP");
-
+app.get('/', function (req, res) {
+    res.send('Simple Web Application is UP')
 });
 
 app.listen(8081, function () {
-
-console.log("Simple Web Application running on port 8081!");
+console.log('Simple Web Application running on port 8081!')
 });
-
 
 //https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
