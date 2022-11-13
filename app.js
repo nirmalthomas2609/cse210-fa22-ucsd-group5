d375ec59aac82f16650bc38c2fc23ad60b531831
anshulshah99 <ayshah@ucsd.edu>
2022-11-13 12:03:44 -0800

    Fixed more styling issues

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let express = require("express");
+const express = require('express');
 
-let app = express();
+const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Simple Web Application is UP')
-});
+  res.send('Simple Web Application is UP')
+})
 
 app.listen(8081, function () {
-console.log('Simple Web Application running on port 8081!')
-});
+  console.log('Simple Web Application running on port 8081!')
+})
 
-//https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
+// https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
