cfee50c7b4a7b2ffcbbc75fb3c5dfb4e4910bf72
Mayank Sharan <mayanksharan96@gmail.com>
2022-12-08 09:45:21 -0800

    Adding Mono and ND install to script

--- build_scripts/test.sh
+++ build_scripts/test.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "Test Worked"
+echo "ND Build start"
+
+# Install Mono
+
+sudo apt install gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+
+sudo apt install mono-devel
+
+echo "Mono installed"
+
+# Install Naturaldocs
+
+curl https://www.naturaldocs.org/download --output NaturalDocs.zip
+unzip NaturalDocs.zip -d NaturalDocs
+
+echo "ND downloaded"
